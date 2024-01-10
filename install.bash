e031ed43f9cef506d8af3b9ceff606c2dade2523
deniz <deniz.hofmeister@gmail.com>
2024-01-10 15:19:54 +0100

    output if firacode is installed

--- install.bash
+++ install.bash
@@ -26,13 +26,13 @@ cp -r ./alacritty/* "$dest_dir/"
 echo "Alacritty config installed!"
 
 # Check if FiraCode is installed
-
 font_pattern="Fira Code.*Retina"
 
 if ! fc-list | grep -q "$font_pattern"; then
 	echo "Fira Code not installed. Please install Fira Code if you'd like."
 	echo "sudo apt install fonts-firacode"
-	exit 1
+else
+	echo "Fira Code installed!"
 fi
 
 echo "Done!"
